9e5a52ad3d68c409856060fba96f28d8862500c7
gs-akhan <akhan@gainsight.com>
2016-05-30 20:44:34 +0530

    More features

--- public/javascripts/components/Instagram.jsx
+++ public/javascripts/components/Instagram.jsx
@@ -1,13 +1,14 @@
-
 var React = require("react");
+import {Link} from "react-router";
+
 
 class MyApp extends React.Component {
 
     render() {
         return (
             <div>
-             <h1>Baegrama</h1>
-            {this.props.children}
+             <h1><Link to = "/">Baegram</Link></h1>
+                {this.props.children}
             </div>
         );
     }
--- public/javascripts/components/Single.jsx
+++ public/javascripts/components/Single.jsx
@@ -2,10 +2,12 @@ var React = require("react");
 import {Link} from "react-router";
 class Single extends React.Component {
     render () {
+        var to = "/views/"+(Math.random() * 1000);
         return (
-            <div className = "post">
-
-            </div>
+            <Link to = {to} >
+                <div className = "post">
+                </div>
+            </Link>
         );
     }
 }
